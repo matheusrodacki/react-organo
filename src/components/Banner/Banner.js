8629b99b7f5ab788f6f5bd6cb6b468042fc3a664
Matheus Rodacki <matheus.rodacki@outlook.com>
2024-02-20 02:17:40 -0300

    adiciona component Banner ao app.

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,16 @@
+import './Banner.css';
+
+function Banner() {
+  //JSX
+  return (
+    <header className='banner'>
+      <img
+        src='/images/banner.png'
+        alt='Banner principal da página Organa, contém uma imagem com um time de pessoas dando 
+      as mãos logo ao lodo dos textos organo e Pessoas e Times organizados em sum só lugar. '
+      />
+    </header>
+  );
+}
+
+export default Banner;
