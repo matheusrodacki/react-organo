666f3219bb880cc148ed75449be0f56e48433fbd
Matheus Rodacki <matheus.rodacki@outlook.com>
2024-02-20 01:52:20 -0300

    create react app

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite <code>src/App.js</code> e salve para recarregar.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          Aprenda React
         </a>
       </header>
     </div>
