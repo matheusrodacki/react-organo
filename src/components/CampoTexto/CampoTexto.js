6d07b8d341c70300e4925e6355f9829bd164445b
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-02-22 15:06:33 -0300

    define um estado para o valorInput

--- src/components/CampoTexto/CampoTexto.js
+++ src/components/CampoTexto/CampoTexto.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import './CampoTexto.css';
 
 export const CampoTexto = (props) => {
+  //let valorInput = 'Guilherme Silveira';
+
+  const [valorInput, setValorInput] = useState('Guilherme Silveira');
+
+  const aoDigitado = (evento) => {
+    setValorInput(evento.target.value);
+    console.log(valorInput);
+  };
+
   return (
     <div className='campo-texto'>
       <label>{props.label}</label>
-      <input required={props.required} placeholder={props.placeholder} />
+      <input value={valorInput} onChange={aoDigitado} required={props.required} placeholder={props.placeholder} />
     </div>
   );
 };
