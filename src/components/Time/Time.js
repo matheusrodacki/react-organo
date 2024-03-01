9267d543ecfc78ea104c891fbb67fc6966ff00cf
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-03-01 18:59:07 -0300

    feat: inicia o projeto com uma lista de colaboradores

--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -7,15 +7,17 @@ export const Time = (props) => {
       <section className='time' style={{ backgroundColor: props.corSecundaria }}>
         <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
         <div className='colaboradores'>
-          {props.colaboradores.map((colaborador) => (
-            <Colaborador
-              key={colaborador.nome}
-              nome={colaborador.nome}
-              cargo={colaborador.cargo}
-              imagem={colaborador.imagem}
-              corDeFundo={props.corPrimaria}
-            />
-          ))}
+          {props.colaboradores.map((colaborador) => {
+            return (
+              <Colaborador
+                key={colaborador.nome}
+                nome={colaborador.nome}
+                cargo={colaborador.cargo}
+                imagem={colaborador.imagem}
+                corDeFundo={props.corPrimaria}
+              />
+            );
+          })}
         </div>
       </section>
     )
