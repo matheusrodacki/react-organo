7333cdad0b8a1aa0e0c7cf3d88a99496b223c5b7
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-03-01 19:14:17 -0300

    feat: encapsula o aray de time na passagem de parametros de app.js

--- src/App.js
+++ src/App.js
@@ -204,15 +204,14 @@ function App() {
         times={times.map((time) => time.nome)}
         aoColaboradorCadastrado={(colaborador) => aoNovoColaboradorAdicionado(colaborador)}
       />
-      {times.map((time) => (
+      {times.map((time, indice) => (
         <Time
-          key={time.nome}
-          nome={time.nome}
-          corPrimaria={time.corPrimaria}
-          corSecundaria={time.corSecundaria}
+          key={indice}
+          time={time}
           colaboradores={colaboradores.filter((colaborador) => colaborador.time === time.nome)}
         />
       ))}
+
       <Rodape />
     </div>
   );
--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -1,20 +1,21 @@
 import Colaborador from '../Colaborador';
 import './Time.css';
 
-export const Time = (props) => {
+export const Time = ({ time, colaboradores, aoDeletar }) => {
   return (
-    props.colaboradores.length > 0 && (
-      <section className='time' style={{ backgroundColor: props.corSecundaria }}>
-        <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+    colaboradores.length > 0 && (
+      <section className='time' style={{ backgroundColor: time.corSecundaria }}>
+        <h3 style={{ borderColor: time.corPrimaria }}>{time.nome}</h3>
         <div className='colaboradores'>
-          {props.colaboradores.map((colaborador) => {
+          {colaboradores.map((colaborador) => {
             return (
               <Colaborador
                 key={colaborador.nome}
                 nome={colaborador.nome}
                 cargo={colaborador.cargo}
                 imagem={colaborador.imagem}
-                corDeFundo={props.corPrimaria}
+                corDeFundo={time.corPrimaria}
+                aoDeletar={aoDeletar}
               />
             );
           })}
